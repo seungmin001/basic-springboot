36513bd283ec05045cef781a0d1d3b108b98e350
seungmin001 <minnie9808@gmail.com>
2022-01-15 01:58:23 +0900

    [added] 화면 만들기 : mustache 적용
    기본 메인 화면 만듦

---
+++ src/main/java/com/example/smspring/springboot/web/IndexController.java
@@ -0,0 +1,13 @@
+package com.example.smspring.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index"; //mustache 스타터에 의해 "src/main/resources/templates"+return문자열+".mustache"(확장자) 로 전환되어 View Resolver가 처리
+    }
+}
