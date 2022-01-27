6c019a399877198c9eb3c43ba3bc7529ee535436
seungmin001 <minnie9808@gmail.com>
2022-01-27 15:11:39 +0900

    게시글 등록기능 추가
    - index.js 추가 (등록기능)
    - posts-save.mustache (게시글 등록 화면)
    - IndexController.java (게시글 등록 화면 GET URL이동)

--- src/main/java/com/example/smspring/springboot/web/IndexController.java
+++ src/main/java/com/example/smspring/springboot/web/IndexController.java
@@ -10,4 +10,9 @@ public class IndexController {
     public String index() {
         return "index"; //mustache 스타터에 의해 "src/main/resources/templates"+return문자열+".mustache"(확장자) 로 전환되어 View Resolver가 처리
     }
+
+    @GetMapping("/posts/save")
+    public String postsSave(){
+        return "posts-save";
+    }
 }
---
+++ src/main/resources/static/js/app/index.js
@@ -0,0 +1,32 @@
+var main = {
+    init : function () {
+        var _this = this;
+        $('#btn-save').on('click', function () {
+            _this.save();
+        });
+
+    },
+    save : function () {
+        var data = {
+            title: $('#title').val(),
+            author: $('#author').val(),
+            content: $('#content').val()
+        };
+
+        $.ajax({
+            type: 'POST',
+            url: '/api/v1/posts',
+            dataType: 'json',
+            contentType:'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function() {
+            alert('글이 등록되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    }
+};
+// 객체 안에 함수를 선언하는 이유 : 브라우저의 스코프는 공용으로 쓰여 함수 이름 중복 시 덮어쓰기 상황 방지
+// 객체 안에서만 함수가 유효함 //ES6 이나 리액트 등은 프레임워크레벨에서 유효범위 기능 지원
+main.init();
