ebbc20e9ce9cbb5c8a6e5d416666e402a5a6300a
seungmin001 <minnie9808@gmail.com>
2022-02-01 01:07:18 +0900

    add Google login/logout
    - Spring Security
    - 사용자 정보 User Entity 추가
    - 권한 Enum Role 추가
    - HttpSession 이용하여 현재 세션에 사용자 정보 저장 / 화면에 출력할 수 있도록 model에 등록

--- src/main/java/com/example/smspring/springboot/web/IndexController.java
+++ src/main/java/com/example/smspring/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.example.smspring.springboot.web;
 
+import com.example.smspring.springboot.config.auth.dto.SessionUser;
+import com.example.smspring.springboot.domain.user.User;
 import com.example.smspring.springboot.service.posts.PostsService;
 import com.example.smspring.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +10,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) { // model : 서버 템플릿 엔진에서 사용할 수 있는 객체
         model.addAttribute("posts", postsService.findAllDesc()); // find결과를 index.mustache에 전달하도록
+        SessionUser user = (SessionUser) httpSession.getAttribute("user"); // CustomOAuth~에서 저장한 것 가져오기
+        if(user!=null){ // 세션에 값이 있을 때만 모델에 등록. model에 값이 없으면 로그인 버튼이 보이게 됨
+            model.addAttribute("userName", user.getName());
+        }
         return "index"; //mustache 스타터에 의해 "src/main/resources/templates"+return문자열+".mustache"(확장자) 로 전환되어 View Resolver가 처리
     }
 
